b670bb115aa210964564d3ea8084d2a86e2206de
carlosyasu91 <carlosyasu91@gmail.com>
2016-02-01 17:36:17 -0800

    Make server rquest listener function tests pass, but broke server tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,7 +11,7 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
+var http = require("http");
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
@@ -32,6 +32,9 @@ var requestHandler = function(request, response) {
   // The outgoing status.
   var statusCode = 200;
 
+  if(request.method === "POST") {
+    statusCode = 201;
+  }
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
@@ -39,20 +42,23 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
   response.writeHead(statusCode, headers);
-
+  var message = {
+    results : [1,2,3]
+  };
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
   // up in the browser.
   //
+  // response.write(JSON.stringify(message));
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+  response.end(JSON.stringify(message));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -71,3 +77,4 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
+exports.requestHandler = requestHandler;
